b52e875be208942a2ec6b6087bc43947ff57eb82
daniilxt <daniil-firsov@mail.ru>
2022-05-27 00:39:31 +0300

    Edited userProfileInfoServiceImpl

--- src/main/kotlin/com/daniilxt/meetty/dto/UserInfoDto.kt
+++ src/main/kotlin/com/daniilxt/meetty/dto/UserInfoDto.kt
@@ -1,6 +1,10 @@
 package com.daniilxt.meetty.dto
 
+import java.time.LocalDateTime
+
 data class UserInfoDto(
     val userInfo: SimpleUserDto,
-    val userAdditionalInfo: UserAdditionalInfoDto
+    val userAdditionalInfo: UserAdditionalInfoDto,
+    val userAchievements: List<UserAchievementDto>,
+    val lastActivity: LocalDateTime?
 )
--- src/main/kotlin/com/daniilxt/meetty/service/impl/UserInfoServiceImpl.kt
+++ src/main/kotlin/com/daniilxt/meetty/service/impl/UserInfoServiceImpl.kt
@@ -1,10 +1,9 @@
 package com.daniilxt.meetty.service.impl
 
-import com.daniilxt.meetty.dto.UserAdditionalInfoDto
 import com.daniilxt.meetty.dto.UserInfoDto
-import com.daniilxt.meetty.entity.toProfessionalInterestDto
-import com.daniilxt.meetty.entity.toUserDto
-import com.daniilxt.meetty.repository.*
+import com.daniilxt.meetty.dto.UserProfileInfoDto
+import com.daniilxt.meetty.repository.UserRepository
+import com.daniilxt.meetty.service.UserInfoProfileService
 import com.daniilxt.meetty.service.UserInfoService
 import org.springframework.stereotype.Service
 import kotlin.random.Random
@@ -12,24 +11,20 @@ import kotlin.random.nextInt
 
 @Service
 class UserInfoServiceImpl(
-    private val userDetailsRepository: UserDetailsRepository,
     private val userRepository: UserRepository,
-    private val userEducationRepository: UserEducationRepository,
-    private val universityRepository: UniversityRepository,
-    private val userAchievementRepository: UserAchievementRepository,
-    private val userProfessionalInterestRepository: UserProfessionalInterestRepository,
+    private val userInfoProfileService: UserInfoProfileService,
 ) : UserInfoService {
     override fun getAnyUser(userEmail: String): UserInfoDto {
         val users = userRepository.findAll().filter { it.email != userEmail }
         val count = users.count()
         val user = users[Random.nextInt(0 until count)]
-        return UserInfoDto(
-            userInfo = user.toUserDto(),
-            userAdditionalInfo = UserAdditionalInfoDto(
-                professionalInterests = userProfessionalInterestRepository.getByUserId(user.id)
-                    .map { it.toProfessionalInterestDto() },
-                userBirthday = user.birthDay
-            )
-        )
+        return userInfoProfileService.getProfileInfo(user.email).toUserInfoDto()
     }
 }
+
+private fun UserProfileInfoDto.toUserInfoDto() = UserInfoDto(
+    userInfo = userInfo,
+    userAdditionalInfo = userAdditionalInfo,
+    userAchievements = userAchievements,
+    lastActivity = lastActivity
+)
